7b50656536eeda0ebe1cd7b5d6f34eebf422b167
Abdul Hameed T A <hameedamithattuparambil@gmail.com>
2025-10-01 16:00:59 +0530

    Setup Customer Login

--- core/settings.py
+++ core/settings.py
@@ -136,3 +136,10 @@ AUTH_USER_MODEL = 'api.User'
 import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
